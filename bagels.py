d317adf58eeab05ef0f3388f52bb144f6abf9c24
bluedrummer <bluedrummer@github>
2022-01-18 20:54:56 +1100

    New projects

---
+++ bagels.py
@@ -0,0 +1,90 @@
+#Hello thank you for using my program
+#This works with Python3
+#What does this program do?: This is a deductive logic game
+#What is the game rules: The program creates a random 3 digit number try to guess what it is here are some hints!
+#Pico: One digit is correct but in the wrong position.
+#Fermi: One digit is correct and in the right position.
+#Bagels: No digit is correct
+#This is project 1 or chapter 1 from "THE BIG BOOK OF SMALL PYHTON PROJECTS"
+#Thank you for using!
+
+
+from random import randint
+print("Bagels, a deductive logic game.")
+print("By bluedrummer  or https://github.com/bluedrummer")
+print()
+print("I am thinking of a digit 3-digit number. Try to guess what it is.")
+print("Here are some clues:")
+print("When I say:     That means:")
+print("  Pico          One digit is correct but in the wrong position.")
+print("  Fermi         One digit is correct and in the right position.")
+print("  Bagels        No digit is correct.")
+print("I have thought up a number.")
+print("You have 10 guesses to get it.")
+playing_status = True
+while playing_status == True:
+    playing_status = True
+    number = 0
+    while True:
+        good_number = False
+        number = randint(100, 987)
+        number = str(number)
+        good_number_count = 0
+        for i in range(0, len(number)):
+            if number.count(number[i]) == 1:
+                good_number_count += 1
+            if good_number_count == 3:
+                good_number = True
+                break
+        if good_number == True:
+            break
+
+    for i in range(0, 10):
+        print(f"Guess #{i + 1}:")
+        while True:
+            guess_number = input()
+            if len(guess_number) == 3:
+                break
+            if len(guess_number) != 3:
+                print("Please retry as number is not 3 digits")
+        type_of_guess = ""
+        digits_guessed_right=0
+        for l in range(0, len(number)):
+            for d in range(0, len(number)):
+                if guess_number[l] == number[d]:
+                    digits_guessed_right += 1
+                    if l == d:
+                        type_of_guess = type_of_guess + " " + "Fermi"
+                        break
+                    else:
+                        type_of_guess = type_of_guess + " " + "Pico"
+                        break
+
+        if digits_guessed_right == 0:
+            type_of_guess = "Bagels"
+        print(type_of_guess.lstrip())
+
+        if guess_number == number:
+            print("You got it!")
+            print("Would you like to play again? (yes or no)")
+            player_status = input().lower()
+            if player_status == "no":
+                print("Thanks for playing!")
+                print("bluedrummer at github")
+                playing_status = False
+                break
+            if player_status == "yes":
+                break
+
+        if i == 9 and guess_number != number:
+            print("You didint get the number.")
+            print(f"The number was {number}")
+            print("Would you like to play again? (yes or no)")
+            player_status = input().lower()
+            if player_status == "no":
+                print("Thanks for playing!")
+                print("bluedrummer at github")
+                playing_status = False
+                break
+            if player_status == "yes":
+                break
---
+++ birthday_paradox.py
@@ -0,0 +1,77 @@
+#Hello thank you for using my program
+#This works with Python3
+#What does this program do?: This program performs several probability experimints to determine the percentages of people who have the same birthday in N sized group
+#This is project 2 or chapter 2 from "THE BIG BOOK OF SMALL PYHTON PROJECTS"
+#Thank you for using!
+import datetime, random
+print("Birthday Paradox, by bledrummer or https://github.com/bluedrummer")
+print("Explore the surprising probabilities of the Birthday Paradox")
+print("There are no leap years")
+print("Here is a test")
+print()
+print("How many birthdays shall i generate? (Max 100)")
+birthay_count = 0
+while True:
+    birthday_count = int(input())
+    if birthday_count < 100 and birthday_count:
+        break
+    print("Please retry as the number of birthdays you like me to generate is to much.")
+print()
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+birthdays = []
+
+def generate_random_birthdays(number_of_birthdays):
+
+
+    birthdays_generated = []
+    for i in range(0, number_of_birthdays):
+        start_of_year = datetime.date(2001, 1, 1)
+        random_number_of_days = datetime.timedelta(random.randint(0, 364))
+        birthday = start_of_year + random_number_of_days
+        month = birthday.month
+        birthdays_generated.append(f"{months[month-1]} {birthday.day}")
+    return birthdays_generated
+
+def getMatch(birthdays_list):
+    count = 0
+    if len(birthdays_list) == len(set(birthdays_list)):
+        return None
+    for a in range(0, len(birthdays_list)):
+        count = 0
+        for b in range(0, len(birthdays_list)):
+            if birthdays_list[a] == birthdays_list[b]:
+                count += 1
+                if count == 2:
+                    return birthdays_list[a]
+
+
+birthdays = generate_random_birthdays(birthday_count)
+
+print(f"Here are {birthday_count} birthdays")
+print(birthdays)
+match = getMatch(birthdays)
+if match == None:
+    print("In this sample simulation, nobody has the same birthday.")
+if match != None:
+    print(f"In this sample simulation, multiple people have a birthday on {match}.")
+print()
+print("How many simulations would you like to run? Note: If the desired amount of simulations is over 10,000 may take more take more then 60 seconds so please do not quit the code")
+simulation_count = int(input())
+print(f"Generating {birthday_count} random birthdays {simulation_count} times")
+full_count = 0
+for i in range(0, simulation_count):
+    birthdays = generate_random_birthdays(birthday_count)
+    match = getMatch(birthdays)
+    if match != None:
+        full_count += 1
+    print(f"{i  + 1 } simulations complete.")
+calculation = full_count / simulation_count * 100
+print("The simulations are done!")
+print()
+print("Here are the results:")
+print(f"Out of {simulation_count} simulations of {birthday_count} people, there was a")
+print(f"matching birthday in that group {full_count} times. This means")
+print(f"that {birthday_count} people have a {calculation} % chance of")
+print("having a matching birthday in their group.")
+print("Thats probably more then what you think!")
+print("Thanks for using (bluedrummer at github)")
